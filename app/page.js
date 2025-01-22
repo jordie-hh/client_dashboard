78a906a51f752ea124b7218c328e9021e0ae9f82
jordie-hh <jordievolkerink@happyhorizon.com>
2025-01-22 15:27:14 +0100

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,10 @@
+'use client';
+import ClientDashboard from './components/ClientDashboard';
+
+export default function Home() {
+  return (
+    <main className="min-h-screen">
+      <ClientDashboard />
+    </main>
+  );
+}
